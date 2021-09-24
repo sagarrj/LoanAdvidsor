f9f40a6ebe911a858d36e2689c435eb67790ed1c
Sagar Jaisinghani <sagar-rajkumar.jaisinghani@capgemini.com>
2021-09-24 15:27:34 +0530

    Init Code

---
+++ src/main/java/com/finance/LoanAdvisor/LoanAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.finance.LoanAdvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanAdvisorApplication.class, args);
+	}
+
+}
